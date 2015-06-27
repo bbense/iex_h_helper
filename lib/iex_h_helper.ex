9da54930f5f7f0eb5a3284f825c6dbc7880e56f0
Booker C. Bense <bbense@gmail.com>
2015-06-27 08:19:05 -0700

    wip

--- lib/iex_h_helper.ex
+++ lib/iex_h_helper.ex
@@ -4,8 +4,11 @@ defmodule Iex.HHelper do
 
 	All of the functions for this behaviour return one of the following tuples. 
 
-	{:found, doc_list }  - Documentation found help command prints list of {header,doc} tuples.
-	{:not_found, doc_list } - Documentation not found, but should have been found by module.
+	{:found, doc_list }  - Documentation found doc_list is a list {header,doc} tuples.
+	                       in markdown format.
+
+	{:not_found, doc_list } - Documentation not found, but could have been found by module if 
+	                          it existed.
 	{:can_not_help, doc_list} - Module does not know how to find documentation for arguements. 
 
 	The intent is that helpers can be stacked and the iex command can do either `:first` or `:all`
@@ -16,7 +19,28 @@ defmodule Iex.HHelper do
 	use Behaviour 
 
 	@doc """
-	h(module) when is_atom(module)
+	document(module) when is_atom(module)
 	"""
-	defcallback h(module) :: { Atom , list }
+	defcallback document(module) :: { Atom , list }
+
+	@doc """
+	document(module,function) when is_atom(module) and is_atom(function)
+	"""
+	defcallback document(module,function) :: { Atom, list } 
+
+	@doc """
+     document(modules, function) when is_list(modules) and is_atom(function)
+	"""
+	defcallback document(module_list,function) :: {Atom, list}
+
+	@doc """
+	 document(modules, function, arity) when is_list(modules) and is_atom(function) and is_integer(arity)
+	"""
+	defcallback document(module_list,function,arity) :: {Atom, list}
+
+	@doc """
+	 document(module, function, arity) when is_atom(module) and is_atom(function) and is_integer(arity)
+	"""
+	defcallback document(module,function,arity) :: {Atom, list}
+
 end
